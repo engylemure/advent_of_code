ec03ea3998f9f09188bd24e36e0c203d479d213d
engylemure <jordao.rosario01@gmail.com>
2021-03-03 00:00:11 -0300

    [2020 day 09] Part A Solution

--- 2020/day_10/src/main.rs
+++ 2020/day_10/src/main.rs
@@ -1,3 +1,64 @@
+use adapter_array::*;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input");
+    println!("{:?}", solution_01(transform_input(input)));
+    println!("{:?}", solution_02(transform_input(input)));
+}
+
+mod adapter_array {
+    #[derive(Debug)]
+    pub struct AdapterBag {
+        adapters: Vec<u64>,
+    }
+
+    impl AdapterBag {
+        fn parse(input: &str) -> Self {
+            let mut adapters: Vec<u64> = input.lines().map(str::parse).flatten().collect();
+            adapters.sort();
+            adapters.push(adapters[adapters.len() - 1] + 3);
+            Self { adapters }
+        }
+    }
+
+    pub fn solution_01(input: AdapterBag) -> u32 {
+        let mut diffs = (0, 0, 0);
+        let mut last_value = 0;
+        for i in 0..input.adapters.len() {
+            let value = input.adapters[i];
+            match value - last_value {
+                1 => diffs.0 += 1,
+                2 => diffs.1 += 1,
+                3 => diffs.2 += 1,
+                _ => unreachable!(),
+            }
+            last_value = value;
+        }
+        diffs.0 * diffs.2
+    }
+
+    pub fn solution_02(input: AdapterBag) -> u64 {
+        0
+    }
+
+    pub fn transform_input(input: &str) -> AdapterBag {
+        AdapterBag::parse(input)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::adapter_array::*;
+
+    #[test]
+    fn test_01() {
+        let input = include_str!("example_input");
+        assert_eq!(solution_01(transform_input(input)), 220);
+    }
+
+    #[test]
+    fn test_02() {
+        let input = include_str!("example_input");
+        assert_eq!(solution_02(transform_input(input)), 0);
+    }
 }
